dd0342eab0887dd545dfab63d6642bf9807e415b
Andreas Bergman <andreas@dontping.me>
2015-01-11 16:27:22 +0100

    Some cleanup

--- PHP/CreateCluster/README.md
+++ PHP/CreateCluster/README.md
@@ -4,6 +4,6 @@ This is a simple example on how to automatically create a server cluster using G
 The examples fetches a reserved IP, creates a loadbalancer, a number of servers and adds the servers to the loadbalancer.
 
 The next step would be to install software needed on the servers, this could be done by using the https://github.com/GleSYS/wp-auto-deploy
-The code shoule be self explanatory.
+The code should be self explanatory.
 
 The API key will need to have access rights to IP, loadbalancer and server.
--- PHP/CreateCluster/functions.php
+++ PHP/CreateCluster/functions.php
@@ -2,28 +2,31 @@
 
 include('php-restclient-master/restclient.php');
 
-function get_reserved_ip(){
+function get_reserved_ip()
+{
 	$data = array(
 		'used'=>0	
 	);
 	$ips = _call('ip/listown',$data);
 
-	if(count($ips->response->iplist) < 1){	
+	if(count($ips->response->iplist) < 1) {	
 		return false;
-	}else{
+	} else {
 		$ip = $ips->response->iplist[0];
 		return $ip;
 	}
 }
 
-function create_loadbalancer($ip,$datacenter){
+function create_loadbalancer($ip,$datacenter)
+{
 	$data = array('name'=>'Demo-LB','datacenter'=>$datacenter,'ipaddress'=>$ip);
 	$lb = _call('loadbalancer/create',$data);
 
 	return $lb->response->loadbalancer;
 }
 
-function create_server($datacenter,$hostname,$platform = 'OpenVZ',$template = 'Debian 7.0 64-bit',$disk = '10',$memory='1024',$cpu = '1',$rootpassword = 'Test123!'){
+function create_server($datacenter,$hostname,$platform = 'OpenVZ',$template = 'Debian 7.0 64-bit',$disk = '10',$memory='1024',$cpu = '1',$rootpassword = 'Test123!')
+{
 	$data = array(
 			'datacenter' => $datacenter, 'platform'=>$platform,'hostname' =>$hostname,'templatename'=>$template,'disksize'=>$disk,'memorysize'=>$memory,'cpucores'=>$cpu,'rootpassword'=>$rootpassword
 		);
@@ -33,7 +36,8 @@ function create_server($datacenter,$hostname,$platform = 'OpenVZ',$template = 'D
 
 }
 
-function add_backend_frontend($name,$loadbalancerid,$mode,$stickysession,$port){
+function add_backend_frontend($name,$loadbalancerid,$mode,$stickysession,$port)
+{
 
 	//Create backend
 		$data = array(
@@ -59,10 +63,11 @@ function add_backend_frontend($name,$loadbalancerid,$mode,$stickysession,$port){
 
 }
 
-function add_targets($loadbalancerid, $backendname, $targets, $port){
+function add_targets($loadbalancerid, $backendname, $targets, $port)
+{
 	$i = 0;
 
-	foreach($targets as $target){
+	foreach($targets as $target) {
 
 		$data = array(
 			'loadbalancerid' => $loadbalancerid,
@@ -81,7 +86,8 @@ function add_targets($loadbalancerid, $backendname, $targets, $port){
 	return $target_data;
 }
 
-function _call($end_point,$payload){
+function _call($end_point,$payload)
+{
 
 	global $user;
         global $key;
