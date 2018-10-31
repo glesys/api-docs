1bf2df78966462222f9f078e3c4dfd1a74082b99
Martin Bagge <martin.bagge@glesys.se>
2018-10-31 08:05:15 +0100

    Imported script from Marcus Månsson at gitlab.
    Closes #32

--- BASH/LetsencryptGlesysHook/glesys-dns-01-hook.sh
+++ BASH/LetsencryptGlesysHook/glesys-dns-01-hook.sh
@@ -1,84 +1,162 @@
 #!/usr/bin/env bash
-
-###########
 #
-# - dependencies: curl xmlstarlet (debian: apt-get install curl xmlstarlet)
-# - GleSYS API credentials (DOMAIN permissions)
-#   syntax:
-#   echo "export USER=CL12345" > /etc/ssl/private/.glesys-credentials
-#   echo "export KEY=KEY_GOES_HERE" >> /etc/ssl/private/.glesys-credentials
+#   An improved hook script for dehydrated and GleSYS API.
 #
-###########
-
-DEPS=`whereis xmlstarlet`
-if [ "$?" -ne 0 ]; then
-        echo "install xmlstarlet"
-        exit 1
-fi
-DEPS=`whereis curl`
-if [ "$?" -ne 0 ]; then
-        echo "install curl"
-        exit 1
-fi
-
+#   Original version by kiranos:
+#   https://github.com/kiranos/API/tree/master/BASH/LetsencryptGlesysHook
+#
+#   This fork is maintained by marcusmansson:
+#   https://gitlab.com/marcusmansson/glesys-dns01
+#
+#   Dependencies
+#   ============
+#   - curl and xmlstarlet (debian: apt-get install curl xmlstarlet)
+#   - GleSYS API credentials (DOMAIN permissions for list, add, remove records)
+#
+#   Prerequisites
+#   =============
+#   Read README.md
+#
+#   echo "export USER=CL12345" > /etc/ssl/private/glesys-credentials
+#   echo "export KEY=KEY_GOES_HERE" >> /etc/ssl/private/glesys-credentials
+#   chmod 600 /etc/ssl/private/glesys-credentials
+#
+#   IMPORTANT
+#   =========
+#   Edit your dehydrated config and set CHAIN="yes", otherwise wildcard urls
+#   will not work if you create a combo cert for both domain.tld and *.domain.tld.
+#
+#   CHAIN="yes"
+#
+#
 set -e
-set -u
 set -o pipefail
 umask 077
+HOOK=$1
+TIME=$(date)
+LOGFILE=/var/log/glesys-dns-01.log
+VERBOSE=
 
-#Load GleSYS Credentials
-. /etc/ssl/private/.glesys-credentials
+_cleanup () {
+    rm -f /tmp/api-log.xml
+}
+trap _cleanup EXIT
 
-#split domain
-FQDN=$2
-DOMAIN=`echo $2 |rev |cut -d '.' -f1-2 |rev`
-SUBDOMAIN=`echo $2 |rev |cut -d '.' -f3- |rev`
-DONE="no"
-#Functions
-function validate_xml {
-#Check if API call got status 200 (OK)
-STATUSCODE=`xmlstarlet sel -t -v "/response/status/code" /tmp/api-log.xml`
-if [ "$STATUSCODE" -ne 200 ]; then
-        ERRORCODE=`xmlstarlet sel -t -v "/response/status/text" /tmp/api-log.xml`
-        echo "Error: $ERRORCODE"
-        exit 1
+# Tail logfile in background (if interactive)
+if [[ $VERBOSE ]] && test -t 0; then
+    tail --pid $$ -f $LOGFILE &
 fi
+
+# Check required bins.
+hash xmlstarlet
+hash curl
+
+# Load GleSYS Credentials.
+source /etc/ssl/private/glesys-credentials
+
+# Parse all domain challenges from argument list.
+domains=();
+_parse_domains () {
+    shift;
+    while (( "$#" )); do
+        read -r FQDN _ CHALLENGE _ <<< "$@"
+        shift 3
+        # convert fqdn to array and get the last two slices
+        x=(${FQDN//./ })
+        x="${x[@]: -2:2}"
+        DOMAIN=${x// /.}
+        domains+=("$DOMAIN $FQDN $CHALLENGE")
+    done
 }
 
-if [[ "$1" = "deploy_challenge" ]]; then
-        ##Create TXT-Record for LetsEncrypt
-        curl -sS -X POST --data-urlencode domainname="$DOMAIN" --data-urlencode host="_acme-challenge.$FQDN." --data-urlencode type="TXT" --data-urlencode data="$4" --data-urlencode ttl="300" -k --basic -u $USER:$KEY https://api.glesys.com/domain/addrecord/ > /tmp/api-log.xml
-        ##Run function to validate the response
-        validate_xml
-        DONE="yes"
-fi
+_start_log () {
+    echo $TIME: "$@"
+    echo $TIME: $(declare -p domains)
+} >> $LOGFILE
 
-if [[ "$1" = "clean_challenge" ]]; then
-        #API call to retrieve list of records for the domain.
-        curl -sS -X POST --data-urlencode domainname="$DOMAIN" -k --basic -u $USER:$KEY https://api.glesys.com/domain/listrecords/ > /tmp/api-log.xml
-        #Run function to validate the response
-        validate_xml
-                #remove TXT records created by this script
-                for i in `xmlstarlet sel -t -c /response/records/item[host="'"_acme-challenge.$FQDN".'"] -f /tmp/api-log.xml |grep "<recordid>" |grep -o '[0-9]\+'`
-                do
-                        curl -sS -X POST --data-urlencode recordid="$i" -k --basic -u $USER:$KEY https://api.glesys.com/domain/deleterecord/ > /tmp/api-log.xml
-                        validate_xml
-                done
-        DONE="yes"
-fi
+_validate_response () {
+    # Check if API call got status 200 (OK).
+    STATUSCODE=$(xmlstarlet sel -t -v "/response/status/code" /tmp/api-log.xml)
 
-if [[ "$1" = "deploy_cert" ]]; then
-    # do nothing for now
-    # but can be replaced by for example reload apache2
-    # systemctl reload apache2
-    DONE="yes"
-fi
+    if [[ $STATUSCODE -ne 200 ]]; then
+        ERRORCODE=$(xmlstarlet sel -t -v "/response/status/text" /tmp/api-log.xml)
+        echo "Error in $0: $ERRORCODE"
+        exit 1
+    fi
+}
 
-rm -f /tmp/api-log.xml
+deploy_challenge () {
+    _parse_domains $@
 
-if [[ ! "$DONE" = "yes" ]]; then
-    echo Unkown hook "$1"
-    exit 1
-fi
+    # Create TXT records for all ACME challenges.
+    for domain in "${domains[@]}"; do
+        read DOMAIN FQDN CHALLENGE <<< "$domain"
+        glesys_api addrecord domainname=$DOMAIN \
+            host=_acme-challenge.$FQDN. type=TXT ttl=300 data=$CHALLENGE
+    done
 
+    # Wait for settings to apply on the endpoint.
+    sleep 2
+}
+
+clean_challenge () {
+    _parse_domains $@
+    local lastdomain=""
+
+    # For all hosts:
+    for domain in "${domains[@]}"; do
+        read DOMAIN FQDN CHALLENGE <<< "$domain"
+        [[ $DOMAIN = $lastdomain ]] && continue
+        lastdomain=$DOMAIN
+
+        # API call to retrieve list of records for the domain.
+        glesys_api listrecords domainname=$DOMAIN
+
+        acme_records=$(xmlstarlet sel -t \
+            -m "//host[contains(., '_acme-challenge')]/.." \
+            -v "concat(recordid, ' ')" \
+            -n /tmp/api-log.xml)
+
+        # Remove TXT records created by this script.
+        for id in $acme_records; do
+            glesys_api deleterecord recordid=$id
+        done
+    done
+}
+
+exit_hook () {
+    # - You might want to restart your web server here or
+    # - Truncate log file, since no errors occured
+    :
+}
+
+# Prefix all words using first argument as prefix.
+_prefix () {
+    local prefix=$1; shift
+    printf "%s" "${@/#/$prefix}"
+}
+
+# API query parts.
+CURL="curl -sS -X POST -k --basic"
+API_ENDPOINT="--url https://api.glesys.com/domain"
+glesys_api () {
+    local METHOD=$1; shift
+
+    # Add --data-urlencode to all arguments.
+    local PARAMS=$(_prefix " --data-urlencode " $@)
+
+    # Log query.
+    echo "\n$TIME: $CURL $API_ENDPOINT/$METHOD/ $PARAMS"
+
+    # Perform actual query in a safe manner.
+    $CURL $API_ENDPOINT/$METHOD/ $PARAMS -K- <<< "-u $USER:$KEY" | tee /tmp/api-log.xml
+
+    # Make sure it succeeded.
+    _validate_response
+} >> $LOGFILE
+
+_start_log $@
+
+# Run hook (if defined).
+declare -f $HOOK > /dev/null && $HOOK $@
 exit 0
